c4ae480625a4ecb220e6f726a42dd62282d88075
Developer <dev@example.com>
2026-06-11 15:13:54 +0300

    Remove duplicate cubit and state files - keep clean architecture versions

--- lib/features/setting/logic/cubit/settings_cubit.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-class SettingsCubit extends Cubit<bool> {
-  SettingsCubit() : super(true);
-
-  void toggleNotifications(bool value) {
-    emit(value);
-  }
-}
